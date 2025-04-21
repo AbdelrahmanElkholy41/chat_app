e349798abdeee20b793b32c5542c90dc72c3f9e4
Abdelrahman Elkhoily <abdelrahmanelkholy98@gmail.com>
2025-04-21 02:21:29 +0200

    create cubit and state

--- lib/Cubit/login_state.dart
+++ lib/Cubit/login_state.dart
@@ -4,3 +4,6 @@ part of 'login_cubit.dart';
 sealed class LoginState {}
 
 final class LoginInitial extends LoginState {}
+final class LoginSuccess extends LoginState {}
+final class LoginLoading extends LoginState {}
+final class LoginFailuer extends LoginState {}
